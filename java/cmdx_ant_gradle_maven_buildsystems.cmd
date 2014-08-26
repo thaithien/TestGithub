69b6b1637640ef6e248c7a25f976f44d94243e3d
thaithien <ngththien@gmail.com>
2014-08-26 20:29:58 +0700

    Added cmdx_ant_gradle_maven_buildsystems.cmd

---
+++ java/cmdx_ant_gradle_maven_buildsystems.cmd
@@ -0,0 +1,12 @@
+REM set PATH=
+set ANT_HOME=%CD%\apache-ant-1.9.4
+set MAVEN_HOME=%CD%\apache-maven-3.2.3
+set GRADLE_HOME=%CD%\gradle-2.0
+rem D:\00_ProgramFiles\java_buildsystems\apache-ant-1.9.4\bin
+rem set PATH=%CD%\
+echo %ANT_HOME%
+echo %MAVEN_HOME%
+echo %GRADLE_HOME%
+set PATH=%ANT_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%PATH%
+cd /D C:\temp
+cmd 
