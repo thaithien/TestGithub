5ec7b5c4ddfba0739c850ead35a9dc3e5807cd13
thaithien <ngththien@gmail.com>
2014-10-22 11:06:43 +0700

    addwed

---
+++ ws/wget_aws.cmd
@@ -0,0 +1,139 @@
+
+::=========================================================
+::Compute & Networking
+::    Amazon EC2
+::    Auto Scaling
+::    Elastic Load Balancing
+::    Amazon VPC
+::    Amazon Route 53
+::    AWS Direct Connect
+::
+::Storage & Content Delivery
+::    Amazon S3
+::    Amazon Glacier
+::    Amazon EBS
+::    AWS Import/Export
+::    AWS Storage Gateway
+::    Amazon CloudFront
+::
+::Database
+::    Amazon RDS
+::    Amazon DynamoDB
+::    Amazon ElastiCache
+::    Amazon Redshift
+::    Amazon SimpleDB
+::
+::Analytics
+::    Amazon EMR
+::    Amazon Kinesis
+::    AWS Data Pipeline
+::
+::Applications
+::    Amazon WorkSpaces
+::    Amazon Zocalo
+::
+::Deployment & Management
+::    AWS Identity & Access Management
+::    AWS CloudTrail
+::    Amazon CloudWatch
+::    AWS Elastic Beanstalk
+::    AWS CloudFormation
+::    AWS OpsWorks
+::    AWS CloudHSM
+::    
+::App Services
+::    Amazon AppStream
+::    Amazon CloudSearch
+::    Amazon Elastic Transcoder
+::    Amazon SES
+::    Amazon SQS
+::    Amazon SWF
+::
+::Mobile Services
+::    Amazon Cognito
+::    Amazon Mobile Analytics
+::    Amazon SNS
+::    AWS Mobile SDK for Android
+::    AWS Mobile SDK for iOS
+::
+::Resources
+::    AWS Billing and Cost Management
+::    AWS Marketplace
+::    AWS Support
+::    AWS Glossary
+::=========================================================
+
+
+
+::wget -c -r -k -m -p -np 
+
+
+::=========================================================
+::Compute & Networking
+::    Amazon EC2
+::    Auto Scaling
+::    Elastic Load Balancing
+::    Amazon VPC
+::    Amazon Route 53
+::    AWS Direct Connect
+::
+::Storage & Content Delivery
+::    Amazon S3
+        wget -c -r -k -m -p -np http://docs.aws.amazon.com/AmazonS3/latest/gsg/GetStartedWithS3.html
+        wget -c -r -k -m -p -np http://docs.aws.amazon.com/AmazonS3/latest/dev/Welcome.html
+        wget -c -r -k -m -p -np http://docs.aws.amazon.com/AmazonS3/latest/API/Welcome.html
+        wget -c -r -k -m -p -np http://docs.aws.amazon.com/AmazonS3/latest/UG/Welcome.html
+        wget -c -r -k -m -p -np http://awsdocs.s3.amazonaws.com/S3/latest/s3-qrc.pdf
+
+::    Amazon Glacier
+::    Amazon EBS
+::    AWS Import/Export
+::    AWS Storage Gateway
+::    Amazon CloudFront
+::
+::Database
+::    Amazon RDS
+::    Amazon DynamoDB
+::    Amazon ElastiCache
+::    Amazon Redshift
+::    Amazon SimpleDB
+::
+::Analytics
+::    Amazon EMR
+::    Amazon Kinesis
+::    AWS Data Pipeline
+::
+::Applications
+::    Amazon WorkSpaces
+::    Amazon Zocalo
+::
+::Deployment & Management
+::    AWS Identity & Access Management
+::    AWS CloudTrail
+::    Amazon CloudWatch
+::    AWS Elastic Beanstalk
+::    AWS CloudFormation
+::    AWS OpsWorks
+::    AWS CloudHSM
+::    
+::App Services
+::    Amazon AppStream
+::    Amazon CloudSearch
+::    Amazon Elastic Transcoder
+::    Amazon SES
+::    Amazon SQS
+::    Amazon SWF
+::
+::Mobile Services
+::    Amazon Cognito
+::    Amazon Mobile Analytics
+::    Amazon SNS
+::    AWS Mobile SDK for Android
+::    AWS Mobile SDK for iOS
+::
+::Resources
+::    AWS Billing and Cost Management
+::    AWS Marketplace
+::    AWS Support
+::    AWS Glossary
+::=========================================================
