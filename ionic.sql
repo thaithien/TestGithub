32ad7b03fcf6800ec8eb32af5648524d1f79e99d
thaithien <ngththien@gmail.com>
2015-02-26 23:14:58 +0700

    add ionic.sql note

---
+++ ionic.sql
@@ -0,0 +1,16 @@
+
+Start New Project
+=================
+ionic start myproject blank
+ionic start myproject tabs
+ionic start myproject sidemenu
+
+
+Run
+=================
+ionic build ios
+ionic emulate ios
+ionic serve
+
+
+
